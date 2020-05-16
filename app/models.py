914e5c41230613e34fea959792b596e3c408899a
Олег Федотов <glareintothespace@gmail.com>
2020-05-16 20:58:51 +0300

    added redactor

--- app/models.py
+++ app/models.py
@@ -5,6 +5,8 @@ Definition of models.
 from django.db import models
 from django.contrib import admin
 from django.contrib.auth.models import User
+from ckeditor.fields import RichTextField
+
 
 
 from datetime import datetime
@@ -14,8 +16,10 @@ from django.core.urlresolvers import reverse
 
 class Blog(models.Model):
     title = models.CharField(max_length = 100, unique_for_date = "posted", verbose_name = "Заголовок")
-    description = models.TextField(verbose_name = "Краткое содержание")
-    content = models.TextField(verbose_name = "Полное содержание" ,)
+    # description = models.TextField(verbose_name = "Краткое содержание")
+    description = RichTextField(verbose_name = "Краткое содержание")
+    
+    content = RichTextField(verbose_name = "Полное содержание" )
     posted = models.DateTimeField(default = datetime.now(),db_index = True, verbose_name = "Опубликована")
     author = models.ForeignKey(User, null=True, blank=True, on_delete = models.SET_NULL, verbose_name = "Автор")
     image = models.FileField(default = 'temp.jpg', verbose_name = "Путь к картинке")
